558a1212df76e9844121e775284fde04e20ee72e
Samarjeet Singh <smrsingh2003@gmail.com>
2025-02-08 23:02:00 -0600

    bug fixed

--- JS/Chess.js
+++ JS/Chess.js
@@ -5,14 +5,14 @@ function insertImage() {
 
         if (image.innerText.length !== 0) {
             if (image.innerText == 'Wpawn' || image.innerText == 'Bpawn') {
-                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="${image.innerText}.png" alt="">`
+                image.innerHTML = `${image.innerText} <img class='allimg allpawn' src="../Images/${image.innerText}.png" alt="">`
                 image.style.cursor = 'pointer'
 
             }
 
             else {
 
-                image.innerHTML = `${image.innerText} <img class='allimg' src="${image.innerText}.png" alt="">`
+                image.innerHTML = `${image.innerText} <img class='allimg' src="../Images/${image.innerText}.png" alt="">`
                 image.style.cursor = 'pointer'
             }
         }
